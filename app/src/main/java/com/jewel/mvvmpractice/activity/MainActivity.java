82f14694d25c7f345b22227f477ba9a5331a26ec
Jewel Rana <tonujewel@gmail.com>
2020-07-16 20:33:44 +0600

    part 4 done

--- app/src/main/java/com/jewel/mvvmpractice/activity/MainActivity.java
+++ app/src/main/java/com/jewel/mvvmpractice/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.jewel.mvvmpractice;
+package com.jewel.mvvmpractice.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.jewel.mvvmpractice.R;
 
 public class MainActivity extends AppCompatActivity {
 
