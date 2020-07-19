6713efb978462b87036adf7e86572ee69fe22bef
Jewel Rana <tonujewel@gmail.com>
2020-07-19 17:41:26 +0600

    recycler implement done

--- app/src/main/java/com/jewel/mvvmpractice/activity/MainActivity.java
+++ app/src/main/java/com/jewel/mvvmpractice/activity/MainActivity.java
@@ -3,11 +3,14 @@ package com.jewel.mvvmpractice.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.jewel.mvvmpractice.R;
+import com.jewel.mvvmpractice.adapter.NoteAdapter;
 import com.jewel.mvvmpractice.room.Note;
 import com.jewel.mvvmpractice.viewmodel.NoteViewModel;
 
@@ -22,13 +25,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerView_note);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                // Update recyclerview
-                Toast.makeText(MainActivity.this, "OnChanged", Toast.LENGTH_SHORT).show();
-
+              adapter.setNotes(notes);
             }
         });
     }
--- app/src/main/java/com/jewel/mvvmpractice/adapter/NoteAdapter.java
+++ app/src/main/java/com/jewel/mvvmpractice/adapter/NoteAdapter.java
@@ -1,4 +1,61 @@
 package com.jewel.mvvmpractice.adapter;
 
-public class NoteAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.jewel.mvvmpractice.R;
+import com.jewel.mvvmpractice.room.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item,parent,false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note currentNotes = notes.get(position);
+        holder.txtTitle.setText(currentNotes.getTitle());
+        holder.txtDescription.setText(currentNotes.getDescription());
+        holder.txtPriority.setText(String.valueOf(currentNotes.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes (List<Note> notes){
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder{
+
+        private TextView txtTitle, txtDescription, txtPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtTitle = itemView.findViewById(R.id.txt_title);
+            txtDescription = itemView.findViewById(R.id.txt_description);
+            txtPriority = itemView.findViewById(R.id.txt_priority);
+        }
+    }
+
 }
